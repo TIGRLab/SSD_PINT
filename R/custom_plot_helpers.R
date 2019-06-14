ea816dd2a4321f1e2930e8f653dfb16dd40c271e
Erin W Dickie <erin.w.dickie@gmail.com>
2019-06-14 17:31:09 -0400

    updating notebooks to reference R helper scripts

---
+++ R/custom_plot_helpers.R
@@ -0,0 +1,139 @@
+
+#' Left section of the raincload plots used in sub-cortical cortical change reporting
+samediff_subcort_raincloud <- function(data, this_subcort_ROI, this_YeoNet, no_ticks = TRUE) {
+  plt <- data %>%
+    mutate(corrtype = factor(vertex_type, levels = c('netmean_pvertex', 'netmean_tvertex', 'netmean_tvolume'),
+                             labels = c("Surface Personalized", "Surface Template", "Volume Template"))) %>%
+    gather(nettype, gvalue, diff_net, same_net) %>%
+    filter(subcort_ROI == this_subcort_ROI, YeoNet == this_YeoNet) %>% 
+    ungroup() %>%
+    ggplot(aes(y = corrtype, x = gvalue, fill = nettype, colour = nettype)) +
+    geom_density_ridges(
+      #jittered_points = TRUE, position = "raincloud",
+      alpha = 0.5, scale = 2,
+      quantile_lines = TRUE, quantiles = 2
+    ) +
+    geom_vline(xintercept = 0) +
+    scale_colour_manual(values = c("#808080", YeoNet_colours[[this_YeoNet]])) +
+    scale_fill_manual(values = c("#808080", YeoNet_colours[[this_YeoNet]])) +
+    scale_x_continuous(limits = c(-0.5, 0.6)) +
+    labs(y = NULL,
+         x = NULL) +
+    theme(legend.position='none')
+  if (no_ticks==TRUE) {
+    plt <- plt + theme(axis.title.x=element_blank(),
+                       axis.text.x=element_blank())
+  } else {
+    plt <- plt + labs(x = "Subcortical-Cortical Correlation (Z)")
+  }
+  return(plt)
+  
+}
+
+#' Right section of the raincload plots used in sub-cortical cortical change reporting
+focus_subcort_raincloud <- function(data, this_subcort_ROI, this_YeoNet, no_ticks = TRUE) {
+  plt <- data %>%
+    mutate(corrtype = factor(vertex_type, levels = c('netmean_pvertex', 'netmean_tvertex', 'netmean_tvolume'),
+                             labels = c("Surface Personalized", "Surface Template", "Volume Template"))) %>%
+    mutate(focus = same_net - diff_net) %>%
+    filter(subcort_ROI == this_subcort_ROI, YeoNet == this_YeoNet) %>% 
+    ungroup() %>%
+    ggplot(aes(y = corrtype, x = focus)) +
+    geom_density_ridges(
+      # jittered_points = TRUE, position = "raincloud",
+      alpha = 0.5, scale = 2,
+      quantile_lines = TRUE, quantiles = 2,
+      fill = YeoNet_colours[[this_YeoNet]],
+      colour = YeoNet_colours[[this_YeoNet]]
+    ) +
+    geom_vline(xintercept = 0) +
+    scale_x_continuous(limits = c(-0.3, 0.6)) +
+    labs(y = NULL,
+         x = NULL) +
+    theme(legend.position='none', 
+          axis.title.y=element_blank(),
+          axis.text.y=element_blank()) 
+  if (no_ticks==TRUE) {
+    plt <- plt + theme(axis.title.x=element_blank(),
+                       axis.text.x=element_blank())
+  } else {
+    plt <- plt + labs(x = "Same - Diff.")
+  }
+  return(plt)
+  
+}
+
+#' Combined left and right section of the raincload plots used in sub-cortical cortical change reporting
+samediff_plus_focus_rainclouds <- function(subject_focus, this_subcort, this_YeoNet, no_ticks = TRUE) {
+  plt <- plot_grid(samediff_subcort_raincloud(subject_focus, this_subcort, this_YeoNet, no_ticks), 
+                   focus_subcort_raincloud(subject_focus, this_subcort, this_YeoNet, no_ticks), 
+                   rel_widths = c(2,1))
+  return(plt)
+}
+
+#' Combined subcortical-cortical correlation rainclouds for one subcortical structure
+subcortical_raincloud <- function(subject_focus, this_subcort) {
+  DM <- samediff_plus_focus_rainclouds(subject_focus, this_subcort, "DM")
+  FP <- samediff_plus_focus_rainclouds(subject_focus,this_subcort, "FP")
+  VA <- samediff_plus_focus_rainclouds(subject_focus,this_subcort, "VA")
+  SM <- samediff_plus_focus_rainclouds(subject_focus,this_subcort, "SM", no_ticks = FALSE)
+  title <- ggdraw() + draw_label(this_subcort, fontface='bold')
+  plt <- plot_grid(title, DM, FP, VA, SM, ncol = 1, rel_heights = c(0.5, 1, 1, 1, 1.5))
+  return(plt)
+}
+
+# figure out where the white lines goes
+hgrid_beaks <- node_annotations %>%
+  filter(etype == "Cort") %>%
+  mutate(diffnet = as.numeric(network) %>% diff()*1:80) %>%
+  filter(diffnet > 0) %>%
+  pull(diffnet)
+
+# make the Yeo 6 network color bar for the axis
+network_bar <- node_annotations %>%
+  filter(etype == "Cort") %>%
+  mutate(to_lab = factor(node_name, levels = rev(node_annotations$node_name))) %>%
+  ggplot(aes(x=1, y=to_lab, fill = network)) +
+  geom_tile() +
+  geom_hline(yintercept= 80.5-hgrid_beaks, color='white', size=1) +
+  scale_fill_manual(values = rev(YeoNet7$hexcode[1:6])) +
+  coord_fixed(ratio = 0.75)
+
+network_top_bar <- node_annotations %>%
+  filter(etype == "Cort") %>%
+  mutate(from_lab = factor(node_name, levels = node_annotations$node_name)) %>%
+  ggplot(aes(x=from_lab, y=1, fill = network)) +
+  geom_tile() +
+  geom_vline(xintercept= hgrid_beaks + 0.5, color='white', size=1) +
+  scale_fill_manual(values = rev(YeoNet7$hexcode[1:6])) +
+  coord_fixed(ratio = 0.75)
+
+# build the full heatmap plot object
+withincortical_heatmap <- function(data, plt_title = "", fillvar = "weight") {
+  fillvar <- enquo(fillvar)
+  max_fill <- 1.15
+  plt <- data %>%
+    inner_join(annotated_graph_edges, by = c("from", "to")) %>%
+    filter(from_to_type == "Cort_Cort") %>%
+    ungroup() %>%
+    select(to, from, !!fillvar) %>%
+    uppertri_df_to_full() %>%
+    mutate(to_lab = factor(to, levels = node_annotations$node_name), 
+           from_lab = factor(from, levels = rev(node_annotations$node_name)),
+           value = if_else(value > max_fill, max_fill, value)) %>%
+    ggplot(aes(x=to_lab, y=from_lab, fill=value)) + 
+    geom_tile() +
+    geom_hline(yintercept= 80.5-hgrid_beaks, color='white', size=1) +
+    geom_vline(xintercept= hgrid_beaks + 0.5, color='white', size=1) +
+    scale_fill_gradient2(high = "#b2182b", mid = "white", low = "#2166ac", 
+                         midpoint = 0, limits = c(-(max_fill), max_fill)) +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1, size = 5),
+          axis.text = element_text(size = 5),
+          panel.border = element_rect(linetype = "solid", color = "black")) +
+    coord_fixed() +
+    labs(title = plt_title,
+         x = NULL, y = NULL, fill = "Correlation (Z)")
+  #p1<- insert_yaxis_grob(plt, network_top_bar, grid::unit(.03, "null"), position = "bottom")
+  p2<- insert_yaxis_grob(plt, network_bar, grid::unit(.03, "null"), position = "left")
+  return(p2)
+}
